cd0b7239504930a084ba6eb61aa28fc814274140
Maxime <maxime.b2494@gmail.com>
2024-03-19 17:34:17 +0100

    first commit

---
+++ src/com/op/p1c1/OpenClassrooms.java
@@ -0,0 +1,37 @@
+package com.op.p1c1;
+
+public class OpenClassrooms {
+	public static void main(String[] args) {
+		
+		int incomes = 500;
+		
+		int savings = 1000;
+		
+		int agePersonne = 19;
+		
+		agePersonne = 33;
+		
+		agePersonne = agePersonne + 27;
+		
+		System.out.println("Age = " + agePersonne + " ans");
+		
+		int epargne = 500;
+	    int revenus = 2000;
+	      //Ajoutez 100 à votre épargne (Yeah!)
+	      epargne = epargne + 100;
+
+	      //Enlevez 50 à votre indemnité (Snif)
+	      revenus = revenus - 50;
+	      //Faites une mise à jour sur votre délai d'épargne
+	      int nombreDeJoursEpargne = (5000 - revenus) / 500;
+	    
+	      //Mettez à jour à nouveau votre indemnité (encore)
+	      revenus = revenus + (30 - 10) * 7;
+	      
+	      String text = "A wonderful string that consists of multiple characters";
+	      
+	      int numberOfVowels = 0;
+	      
+	      double percentageOfVowels = 0.0;
+	}
+}
